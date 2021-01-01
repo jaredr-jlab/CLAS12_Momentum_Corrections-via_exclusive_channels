7df77a6b9deb022e8062b2366d380e3df9201606
Jared Richards <jaredr@jlab.org>
2021-01-01 13:56:31 -0600

    adjust range again

--- mmass_ePipPimPX.cpp
+++ mmass_ePipPimPX.cpp
@@ -21,7 +21,7 @@ int main(int argc, char **argv){
     auto ele = ROOT::Math::PxPyPzMVector();
     auto pro = ROOT::Math::PxPyPzMVector();
 
-    TH1F *h1 = new TH1F("e#pi^{+}#pi^{-}p", "missing mass", 200, 0.0, 0.4);
+    TH1F *h1 = new TH1F("e#pi^{+}#pi^{-}p", "missing mass", 200, 0.0, 0.35);
 
     for(int i = 1; i < argc; i++){
         TFile f1(argv[i]);
